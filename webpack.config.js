9b0bbfed900aa724bfd71e7ad6801140ed73dcee
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-03-23 18:01:25 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,25 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const port = process.env.SERVER_PORT || '3001';
+const port = process.env.PORT || '3000';
 const BUILD_DIR = path.resolve(__dirname, './build');
 const APP_DIR = path.resolve(__dirname, './src');
 
 module.exports = {
     entry: APP_DIR + "/index.js",
     output: {
-        filename: "bundle.[hash].js",
-        path:APP_DIR+'/public'
+        filename: "bundle.[hash].js"
     },
 
     module: {
         rules: [
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true
-                    }
-                }]
-            },
             {
                 test: /.css$/,
                 use: [{
                     loader: 'style-loader'
                 },
                 {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                        camelCase: true,
-                        sourceMap: true
-                    }
+                    loader: 'css-loader'
                 }
                 ]
             },
